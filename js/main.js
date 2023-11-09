dd98dbbbe894490d7d4af113326b87132b450372
Ozodbekorolov23 <ozodbekorolov1234@gmail.com>
2023-11-09 20:00:45 +0500

    timooo

---
+++ js/main.js
@@ -0,0 +1,45 @@
+var Ism  = String( prompt ("Ism yozing"))
+console.log(Ism);
+
+var Familya = String (prompt("Familyani yozing"))
+console.log(Familya)
+
+var Sharif = String (prompt("Sharifni yozing"))
+console.log(Sharif);
+
+var Yosh = Number (prompt("Yoshni yozing"))
+console.log(Yosh);
+
+var Raqam = Number (prompt("Telefon raqamni yozing"))
+console.log(Raqam);
+
+var Manzil = String (prompt("Manzilni yozing"))
+console.log(Manzil);
+
+var Email = String (prompt("E-mail manzilini yozing"))
+console.log(Email);
+
+var Fan = String (prompt("Qiziqqan faningizni yozing"))
+console.log(Fan);
+
+var Maktab = String (prompt("Qaysi maktabda o'qiganingizni yozing"))
+console.log(Maktab);
+
+var Noutbuk = String (prompt("Noutbukingizni nomini yozing"))
+console.log(Noutbuk);
+
+
+var Javob = [
+    "Ism:" + " " + Ism + "\n" +
+    "Familya:" + " " + Familya + "\n" +
+    "Sharif:" + " " + Sharif + "\n" +
+    "Yosh:" + " " + Yosh + "\n" +
+    "Raqam:" + " " + Raqam + "\n" +
+    "Manzil:" + " " + Manzil + "\n" +
+    "Email:" + " " + Email + "\n" +
+    "Fan:" + " " + Fan + "\n" +
+    "Maktab:" + " " + Maktab + "\n" +
+    "Noutbuk:" + " " + Noutbuk + "\n"  
+]
+
+alert(Javob)
