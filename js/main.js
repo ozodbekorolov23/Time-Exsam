965d4bc198dadb3662b79b61b235ebf2daad3a0c
Ozodbekorolov23 <ozodbekorolov1234@gmail.com>
2023-11-11 19:22:26 +0500

    timooo2

--- js/main.js
+++ js/main.js
@@ -1,45 +1,88 @@
-var Ism  = String( prompt ("Ism yozing"))
-console.log(Ism);
+// var Ism  = String( prompt ("Ism yozing"))
+// console.log(Ism);
 
-var Familya = String (prompt("Familyani yozing"))
-console.log(Familya)
+// var Familya = String (prompt("Familyani yozing"))
+// console.log(Familya)
 
-var Sharif = String (prompt("Sharifni yozing"))
-console.log(Sharif);
+// var Sharif = String (prompt("Sharifni yozing"))
+// console.log(Sharif);
 
-var Yosh = Number (prompt("Yoshni yozing"))
-console.log(Yosh);
+// var Yosh = Number (prompt("Yoshni yozing"))
+// console.log(Yosh);
 
-var Raqam = Number (prompt("Telefon raqamni yozing"))
-console.log(Raqam);
+// var Raqam = Number (prompt("Telefon raqamni yozing"))
+// console.log(Raqam);
 
-var Manzil = String (prompt("Manzilni yozing"))
-console.log(Manzil);
+// var Manzil = String (prompt("Manzilni yozing"))
+// console.log(Manzil);
 
-var Email = String (prompt("E-mail manzilini yozing"))
-console.log(Email);
+// var Email = String (prompt("E-mail manzilini yozing"))
+// console.log(Email);
 
-var Fan = String (prompt("Qiziqqan faningizni yozing"))
-console.log(Fan);
+// var Fan = String (prompt("Qiziqqan faningizni yozing"))
+// console.log(Fan);
 
-var Maktab = String (prompt("Qaysi maktabda o'qiganingizni yozing"))
-console.log(Maktab);
+// var Maktab = String (prompt("Qaysi maktabda o'qiganingizni yozing"))
+// console.log(Maktab);
 
-var Noutbuk = String (prompt("Noutbukingizni nomini yozing"))
-console.log(Noutbuk);
+// var Noutbuk = String (prompt("Noutbukingizni nomini yozing"))
+// console.log(Noutbuk);
 
 
-var Javob = [
-    "Ism:" + " " + Ism + "\n" +
-    "Familya:" + " " + Familya + "\n" +
-    "Sharif:" + " " + Sharif + "\n" +
-    "Yosh:" + " " + Yosh + "\n" +
-    "Raqam:" + " " + Raqam + "\n" +
-    "Manzil:" + " " + Manzil + "\n" +
-    "Email:" + " " + Email + "\n" +
-    "Fan:" + " " + Fan + "\n" +
-    "Maktab:" + " " + Maktab + "\n" +
-    "Noutbuk:" + " " + Noutbuk + "\n"  
-]
 
-alert(Javob)
+
+// var Javob = [
+//     "Ism:" + " " + Ism + "\n" +
+//     "Familya:" + " " + Familya + "\n" +
+//     "Sharif:" + " " + Sharif + "\n" +
+//     "Yosh:" + " " + Yosh + "\n" +
+//     "Raqam:" + " " + Raqam + "\n" +
+//     "Manzil:" + " " + Manzil + "\n" +
+//     "Email:" + " " + Email + "\n" +
+//     "Fan:" + " " + Fan + "\n" +
+//     "Maktab:" + " " + Maktab + "\n" +
+//     "Noutbuk:" + " " + Noutbuk + "\n"  
+// ]
+
+// alert(Javob)
+
+
+
+
+var Numbers = 500
+var Numbers1 = 9433.34
+
+console.log(Numbers * Numbers1);
+
+
+var Numbers = 250
+var Numbers1 = 9433.34
+
+console.log(Numbers * Numbers1);
+
+
+var Numbers = 120
+var Numbers1 = 10354.03
+
+ 
+var Numbers = 4716670
+var Numbers1 = 2358335
+var Numbers2 = 1242483.6
+
+console.log(Numbers + Numbers1 + Numbers2);
+
+ var elHeading = document.querySelector(".heading")
+
+var jamiPul = Number (prompt("Qancha yiqqansiz "))
+
+
+if (jamiPul >= 8317488.6 ) {
+        elHeading.textContent = ("Tabriklaymiz OQ yo'l Alisher");}
+
+else {
+        elHeading.textContent = (" Alisher Qishloqqa qayt ozgina sabr qilish kerak bo'larkan."); }
+
+
+
+
+
